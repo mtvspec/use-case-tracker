9d629317e371cffe8c92e0c6c0d337514aad0501
mtvspec <mtv.spec@gmail.com>
2016-08-26 16:18:04 +0600

    adds delete_organization db function

--- db/organizations/e_organization.sql
+++ db/organizations/e_organization.sql
@@ -156,3 +156,42 @@ FROM
 WHERE
   id = v_e_organization_id;
 $$ LANGUAGE sql;
+
+CREATE FUNCTION organizations.delete_organization (
+  IN v_e_organization_id INTEGER,
+  IN v_user_id INTEGER,
+  OUT e_organization_id INTEGER
+)
+AS $$
+WITH upd AS (
+  UPDATE
+    organizations.e_organization
+  SET
+    is_deleted = 'T'
+  WHERE
+    id = v_e_organization_id
+  RETURNING
+    *
+)
+INSERT INTO
+  organizations.e_organization_log (
+    d_operation_type_id,
+    user_id,
+    e_organization_id,
+    bin,
+    short_name,
+    official_name,
+    is_deleted
+  )
+VALUES (
+  3,
+  v_user_id,
+  (SELECT id FROM upd),
+  (SELECT bin FROM upd),
+  (SELECT short_name FROM upd),
+  (SELECT official_name FROM upd),
+  (SELECT is_deleted FROM upd)
+)
+RETURNING
+  e_organization_id;
+$$ LANGUAGE sql;
