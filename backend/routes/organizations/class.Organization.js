9009ad599f571658937d8e821004a914bdad9fdf
mtvspec <mtv.spec@gmail.com>
2016-08-26 20:06:40 +0600

    updates organizations classes

--- backend/routes/organizations/class.Organization.js
+++ backend/routes/organizations/class.Organization.js
@@ -1,169 +1,52 @@
 'use strict';
 
-const ID = require ('./../../common/classes/id');
-const Database = require('./../../db.js');
-const db = new Database();
-const sql = require('./sql.js');
-
-class OrganizationAPI {
-  constructor() {
-    let instance = this;
-    if (instance) {
-      return instance;
-    }
-  }
-  getOrganizations(req, res) {
-    db.selectAllRecords({
-      text: sql.organizations.SELECT_ALL_ORGANIZATIONS(req.User)
-    }, function (response) {
-      if (response && response.status && response.status === 200) {
-        return res.status(response.status).json(response.data).end();
-      } else if (response && response.status && response.status === 204) {
-        return res.status(response.status).end();
+module.exports = class Organization {
+  constructor(data) {
+    let messages = {};
+    let organization = {};
+    if (data) {
+      if (data.bin) {
+        if (data.bin
+          && typeof data.bin === 'string'
+          && data.bin.length === 12) {
+          organization.bin = data.bin;
+        } else {
+          messages.bin = `incorrect 'bin': ${data.bin}`;
+        }
       } else {
-        return res.status(500).end();
+        organization.bin = '';
       }
-    });
-  }
-  getOrganizationByID(req, res) {
-    let organization = new ID(req.params.id);
-    if (organization.id) {
-      db.selectRecordById({
-        text: sql.organizations.SELECT_ORGANIZATION_BY_ID(
-          organization,
-          req.User
-        )
-      }, function (response) {
-        if (response) {
-          return res.status(response.status).json(response.data).end();
+      if (data.shortName) {
+        if (data.shortName
+        && typeof data.shortName === 'string'
+        && data.shortName.length > 1
+        && data.shortName.length <= 1000) {
+          organization.shortName = data.shortName;
         } else {
-          return res.status(500).end();
+          messages.shortName = `incorrect 'shortName': ${data.shortName}`;
         }
-      })
-    } else {
-      return res.status(400).send(`'id' is required`);
-    }
-  }
-  createOrganization(req, res) {
-    let organizationValidationResult = Organization.validate(req.body);
-    let result;
-    if (organizationValidationResult.result) {
-      if (organizationValidationResult.data.bin) {
-        OrganizationAPI.getOrganizationByBIN(req, res, function (response) {
-          if (response && response.status === 200) {
-            return res.status(400).json(`duplicate 'bin': ${req.body.bin}`).end();
-          } else if (response && response.status === 204) {
-            db.insertRecord({
-              text: sql.organizations.INSERT_ORGANIZATION(
-                req.body,
-                req.User)
-            }, function (response) {
-              if (response.status === 201) {
-                return res.status(response.status).json({
-                  id: response.data.create_organization
-                }).end();
-              } else {
-                return res.status(response.status).json(response.data).end();
-              }
-            });
-          }
-        });
+      } else {
+        messages.shortName = `'shortName' is required`;
+      }
+      if (data.officialName) {
+        if (data.officialName
+        && typeof data.officialName === 'string'
+        && data.officialName.length > 1
+        && data.officialName.length <= 4000) {
+          organization.officialName = data.officialName;
+        } else {
+          messages.officialName = `incorrect 'officialName': ${data.officialName}`;
+        }
+      } else {
+        organization.officialName = '';
       }
     } else {
-      res.status(400).json(validationResult.data).end();
+      messages.organization = `'organization' is required`;
     }
-  }
-  updateOrganization(req, res) {
-    let organization = new ID(req.params.id);
-    let organizationValidationResult = Organization.validate(req.body);
-    let result;
-    if (organization.id && organizationValidationResult.result) {
-      req.body.id = idValidationResult.data.id;
-      OrganizationAPI.getOrganizationByBIN(req, res, function (response) {
-        if (response && (response.status === 200 || response.status === 204)) {
-          if (response.id != req.body.id) {
-            db.updateRecord({
-              text: sql.organizations.UPDATE_ORGANIZATION(
-                req.body,
-                req.User
-              )
-            }, function (response) {
-              if (response && response.status === 200) {
-                return res.status(response.status).json({
-                  id: response.data.update_organization
-                }).end();
-              } else if (response.status && response.data) {
-                return res.status(response.status).json(response.data).end();
-              } else {
-                return res.status(500).end();
-              }
-            });
-          } else {
-            return res.status(400).json(`duplicate 'bin': ${req.body.bin}`);
-          }
-        }
-      });
-    } else if (idValidationResult.result === false) {
-      return res.status(400).json(idValidationResult.data).end();
-    } else if (organizationValidationResult.result === false) {
-      return res.status(400).json(organizationValidationResult.data).end();
-    }
-  }
-  deleteOrganization(req, res) {
-    let organization = new ID(req.params.id);
-    if (organization.id) {
-      db.updateRecord({
-        text: sql.organizations.DELETE_ORGANIZATION(
-          idValidationResult.data,
-          req.User
-        )
-      }, function (response) {
-        if (response && response.status === 200) {
-          return res.status(response.status).json({
-            id: response.data.delete_organization
-          }).end();
-        } else {
-          return res.status(response.status).json(response.data).end();
-        }
-      });
-    }
-  }
-  restoreOrganization(req, res) {
-    let organization = new ID(req.params.id);
-    if (organization.id) {
-      db.updateRecord({
-        text: sql.organizations.RESTORE_ORGANIZATION(
-          idValidationResult.data,
-          req.User
-        )
-      }, function (response) {
-        if (response && response.status === 200) {
-          return res.status(response.status).json({
-            id: response.data.restore_organization
-          }).end();
-        } else {
-          return res.status(response.status).json(response.data).end();
-        }
-      });
+    if (Object.keys(messages).length > 0) {
+      return this.messages = messages;
+    } else {
+      return this.organization = organization;
     }
   }
 }
-
-OrganizationAPI.getOrganizationByBIN = function (req, res, cb) {
-  db.selectRecordById({
-    text: sql.organizations.SELECT_ORGANIZATION_BY_BIN(req.body.bin)
-  }, function (response) {
-    if (response && response.status && response.status === 200) {
-      return cb({
-        status: 204,
-        id: response.data.id
-      });
-    } else if (response && response.status && response.status === 204) {
-      return cb({
-        status: 204
-      });
-    }
-  });
-}
-
-module.exports = OrganizationAPI;
--- backend/routes/organizations/index.js
+++ backend/routes/organizations/index.js
@@ -1,66 +1,27 @@
 'use strict';
 
 const router = require('express').Router();
-const Organization = require('./class.Organization.js');
-const org = new Organization();
-
-// remove
-const Database = require('./../../db.js');
-const db = new Database();
-const sql = require('./sql.js');
+const OrganizationAPI = require('./class.OrganizationAPI.js');
+const api = new OrganizationAPI();
 
 router
 .get('/', function (req, res) {
-  org.getOrganizations(req, res);
+  api.getOrganizations(req, res);
 })
 .get('/:id', function (req, res) {
-  if (!req.headers['user-id']) {
-    return res.status(401).end();
-  } else if (isID(req.headers['user-id'])) {
-    let user = {
-      id: Number(req.headers['user-id'])
-    }
-    if (isID(req.params.id)) {
-      let organization = {
-        id: Number(req.params.id)
-      }
-      db.selectRecordById({
-        text: sql.organizations.SELECT_ORGANIZATION_BY_ID(organization, user)
-      }, function (status, data) {
-        if (status && status === 200) {
-          return res.status(status).json(data).end();
-        } else if (status && status === 204) {
-          return res.status(status).end();
-        } else {
-          return res.status(500).end();
-        }
-      });
-    } else {
-      return res.status(400).end(`'OrganizationID' is required`);
-    }
-  } else {
-    return res.status(401).end(`'UserID' must be type of 'INTEGER'`);
-  }
+  api.getOrganizationByID(req, res);
 })
 .post('/', function (req, res) {
-  org.createOrganization(req, res);
+  api.createOrganization(req, res);
 })
 .put('/:id', function (req, res) {
-  org.updateOrganization(req, res);
+  api.updateOrganization(req, res);
 })
 .delete('/:id', function (req, res) {
-  org.deleteOrganization(req, res);
+  api.deleteOrganization(req, res);
 })
 .options('/:id', function (req, res) {
-  org.restoreOrganization(req, res);
+  api.restoreOrganization(req, res);
 })
-
-function isID(id) {
-  if (id && typeof +id === 'number' && Number.isInteger(+id)) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 module.exports = router;
