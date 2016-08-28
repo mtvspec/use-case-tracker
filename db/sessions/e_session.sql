ed5d8b4cab2bc34bee23808c334c6dbc9134fd42
mtvspec <mtv.spec@gmail.com>
2016-08-28 13:27:10 +0600

    adds open_session function specification

--- db/sessions/e_session.sql
+++ db/sessions/e_session.sql
@@ -3,8 +3,24 @@ CREATE TABLE sessions.e_session (
   e_user_id INTEGER NOT NULL,
   open_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   close_timestamp TIMESTAMP WITH TIME ZONE,
-  status_id INTEGER NOT NULL
+  status_id INTEGER NOT NULL DEFAULT 'O',
     PRIMARY KEY (id),
     FOREIGN KEY (e_user_id) REFERENCES users.e_user (id),
     FOREIGN KEY (status_id) REFERENCES sessions.d_session_status (id)
 );
+
+CREATE FUNCTION sessions.open_session (
+  IN v_e_user_id INTEGER,
+  OUT e_session_id INTEGER
+)
+AS $$
+INSERT INTO
+  sessions.e_session (
+    e_user_id
+  )
+VALUES (
+  v_e_user_id
+)
+RETURNING
+  e_session_id;
+$$ LANGUAGE sql;
