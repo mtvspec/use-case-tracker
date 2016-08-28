e154d9da6973a59e364f87df79862b7789a439c2
mtvspec <mtv.spec@gmail.com>
2016-08-28 11:12:18 +0600

    renames attribute description to a_description and removes unique e_person_id constraint

--- db/stakeholders/e_stakeholder.sql
+++ db/stakeholders/e_stakeholder.sql
@@ -1,10 +1,9 @@
 CREATE TABLE stakeholders.e_stakeholder (
   id SERIAL,
   e_person_id INTEGER NOT NULL,
-  description VARCHAR (4000),
-  is_deleted CHAR (1) NOT NULL DEFAULT 'F',
+  a_description VARCHAR (4000),
+    is_deleted CHAR (1) NOT NULL DEFAULT 'F',
       PRIMARY KEY (id),
-      UNIQUE (e_person_id),
       FOREIGN KEY (e_person_id) REFERENCES persons.e_person (id),
       FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
 );
@@ -15,18 +14,18 @@ CREATE TABLE stakeholders.e_stakeholder_log (
   operation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT LOCALTIMESTAMP,
   user_id INTEGER NOT NULL,
   e_person_id INTEGER NOT NULL,
-  description VARCHAR (4000),
-  is_deleted CHAR (1) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (d_operation_type_id) REFERENCES system.d_operation_type (id),
-    FOREIGN KEY (user_id) REFERENCES users.e_user (id),
-    FOREIGN KEY (e_person_id) REFERENCES persons.e_person (id),
-    FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
+  a_description VARCHAR (4000),
+    is_deleted CHAR (1) NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (d_operation_type_id) REFERENCES system.d_operation_type (id),
+      FOREIGN KEY (user_id) REFERENCES users.e_user (id),
+      FOREIGN KEY (e_person_id) REFERENCES persons.e_person (id),
+      FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
 );
 
 CREATE FUNCTION stakeholders.create_stakeholder (
   IN v_e_person_id INTEGER,
-  IN v_description VARCHAR (4000),
+  IN v_a_description VARCHAR (4000),
   IN v_user_id INTEGER,
   OUT e_stakeholder_id INTEGER
 )
@@ -35,11 +34,11 @@ WITH ins AS (
   INSERT INTO
     stakeholders.e_stakeholder (
       e_person_id,
-      description
+      a_description
     )
   VALUES (
     v_e_person_id,
-    v_description
+    v_a_description
   )
   RETURNING
     *
@@ -50,7 +49,7 @@ INSERT INTO
     user_id,
     e_stakeholder_id,
     e_person_id,
-    description,
+    a_description,
     is_deleted
   )
 VALUES (
@@ -58,7 +57,7 @@ VALUES (
   v_user_id,
   (SELECT id FROM ins),
   (SELECT e_person_id FROM ins),
-  (SELECT description FROM ins),
+  (SELECT a_description FROM ins),
   (SELECT is_deleted FROM ins)
 )
 RETURNING
