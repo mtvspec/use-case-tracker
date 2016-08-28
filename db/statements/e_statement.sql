070dc43a10910aeac18d6f23f7e13da120291df9
mtvspec <mtv.spec@gmail.com>
2016-08-28 11:41:44 +0600

    adds default value to d_statement_state_id attribute

--- db/statements/e_statement.sql
+++ db/statements/e_statement.sql
@@ -4,7 +4,7 @@ CREATE TABLE statements.e_statement (
   a_name VARCHAR (1000) NOT NULL,
   a_description VARCHAR (4000),
   d_statement_type_id INTEGER NOT NULL,
-  d_statement_state_id INTEGER NOT NULL,
+  d_statement_state_id INTEGER NOT NULL DEFAULT 1,
     is_deleted CHAR (1) NOT NULL DEFAULT 'F',
       PRIMARY KEY (id),
       FOREIGN KEY (e_stakeholder_id) REFERENCES stakeholders.e_stakeholder (id),
