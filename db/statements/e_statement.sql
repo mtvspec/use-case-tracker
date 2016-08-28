181bc98e602ad5672ebd6f11b816d9629e9340f6
mtvspec <mtv.spec@gmail.com>
2016-08-28 11:34:30 +0600

    updates statement table specification

--- db/statements/e_statement.sql
+++ db/statements/e_statement.sql
@@ -1,12 +1,14 @@
-create table statements.e_statement (
-  id serial,
-  name text not null,
-  description text,
-  d_statement_type_id integer not null,
-  d_statement_state_id integer not null,
-    create_date timestamp with time zone not null default localtimestamp,
-    create_user_id integer not null default 1,
-      primary key (id),
-      foreign key (d_statement_type_id) references statements.d_statement_type (id),
-      foreign key (create_user_id) references users.e_user (id)
+CREATE TABLE statements.e_statement (
+  id SERIAL,
+  e_stakeholder_id INTEGER NOT NULL,
+  a_name VARCHAR (1000) NOT NULL,
+  a_description VARCHAR (4000),
+  d_statement_type_id INTEGER NOT NULL,
+  d_statement_state_id INTEGER NOT NULL,
+    is_deleted CHAR (1) NOT NULL DEFAULT 'F',
+      PRIMARY KEY (id),
+      FOREIGN KEY (e_stakeholder_id) REFERENCES stakeholders.e_stakeholder (id),
+      FOREIGN KEY (d_statement_type_id) REFERENCES statements.d_statement_type (id),
+      FOREIGN KEY (d_statement_state_id) REFERENCES statements.d_statement_state (id),
+      FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
 );
