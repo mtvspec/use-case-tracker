5f8d6c255a7aa0587a09c852245d565d085fc037
mtvspec <mtv.spec@gmail.com>
2016-08-28 12:00:50 +0600

    adds create_statement db function

--- db/statements/e_statement.sql
+++ db/statements/e_statement.sql
@@ -34,3 +34,57 @@ CREATE TABLE statements.e_statement_log (
       FOREIGN KEY (d_statement_state_id) REFERENCES statements.d_statement_state (id),
       FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
  );
+
+CREATE FUNCTION statements.create_statement (
+  IN v_e_stakeholder_id INTEGER,
+  IN v_create_date DATE,
+  IN v_a_name VARCHAR (1000),
+  IN v_a_description VARCHAR (4000),
+  IN v_d_statement_type_id INTEGER,
+  IN v_user INTEGER,
+  OUT e_statement_id INTEGER
+)
+AS $$
+WITH ins AS (
+  INSERT INTO
+    statements.e_statement (
+      e_stakeholder_id,
+      a_create_date,
+      a_name,
+      a_description,
+      d_statement_type_id
+    )
+  VALUES (
+    v_e_stakeholder_id,
+    v_create_date,
+    v_a_name,
+    v_a_description,
+    v_d_statement_type_id
+  )
+  RETURNING
+    *
+)
+INSERT INTO
+  statements.e_statement_log (
+    d_operation_type_id,
+    user_id,
+    e_stakeholder_id,
+    a_create_date,
+    a_name,
+    a_description,
+    d_statement_type_id,
+    d_statement_state_id
+  )
+VALUES (
+  1,
+  v_user,
+  (SELECT id FROM ins),
+  (SELECT a_create_date FROM ins),
+  (SELECT a_name FROM ins),
+  (SELECT a_description FROM ins),
+  (SELECT d_statement_type_id FROM ins),
+  (SELECT d_statement_state_id FROM ins)
+)
+RETURNING
+  e_statement_id;
+$$ LANGUAGE sql;
