dc1bc7d4fdabfdee873c032f626a398048b6af33
mtvspec <mtv.spec@gmail.com>
2016-08-28 13:41:46 +0600

    changes session status datatype to char

--- db/sessions/e_session.sql
+++ db/sessions/e_session.sql
@@ -3,7 +3,7 @@ CREATE TABLE sessions.e_session (
   e_user_id INTEGER NOT NULL,
   open_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   close_timestamp TIMESTAMP WITH TIME ZONE,
-  status_id INTEGER NOT NULL DEFAULT 'O',
+  status_id CHAR (1) NOT NULL DEFAULT 'O',
     PRIMARY KEY (id),
     FOREIGN KEY (e_user_id) REFERENCES users.e_user (id),
     FOREIGN KEY (status_id) REFERENCES sessions.d_session_status (id)
