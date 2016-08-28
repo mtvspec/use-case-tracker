2919ce24cbe3ec17a834c4c75e4d9f413b33d40f
mtvspec <mtv.spec@gmail.com>
2016-08-28 08:51:47 +0600

    changes project name to short_name and adds update project db function

--- db/projects/e_project.sql
+++ db/projects/e_project.sql
@@ -1,7 +1,7 @@
 CREATE TABLE projects.e_project (
   id SERIAL,
   customer_id INTEGER,
-  name VARCHAR (1000) NOT NULL,
+  short_name VARCHAR (1000) NOT NULL,
   official_name VARCHAR (4000),
   description VARCHAR (4000),
   plan_start_date DATE,
@@ -130,6 +130,85 @@ VALUES
     (SELECT state_id FROM ins),
     (SELECT is_deleted FROM ins)
   )
+RETURNING
+  e_project_id;
+$$ LANGUAGE sql;
+
+CREATE FUNCTION projects.update_project (
+  IN v_user_id INTEGER,
+  IN v_project_id INTEGER,
+  IN v_customer_id INTEGER,
+  IN v_short_name VARCHAR (1000),
+  IN v_official_name VARCHAR (4000),
+  IN v_description VARCHAR (4000),
+  IN v_plan_start_date DATE,
+  IN v_plan_end_date DATE,
+  IN v_plan_budget NUMERIC,
+  IN v_fact_start_date DATE,
+  IN v_fact_end_date DATE,
+  IN v_fact_budget NUMERIC,
+  IN v_project_manager_id INTEGER,
+  IN v_state_id INTEGER,
+  OUT e_project_id INTEGER
+)
+AS $$
+WITH upd AS (
+  UPDATE
+    projects.e_project
+  SET
+    customer_id = v_customer_id,
+    short_name = v_short_name,
+    official_name = v_official_name,
+    description = v_description,
+    plan_start_date = v_plan_start_date,
+    plan_end_date = v_plan_end_date,
+    plan_budget = v_plan_budget,
+    fact_start_date = v_fact_start_date,
+    fact_end_date = v_fact_end_date,
+    fact_budget = v_fact_budget,
+    project_manager_id = v_project_manager_id,
+    state_id = v_state_id
+  WHERE
+    id = v_project_id
+  RETURNING
+    *
+)
+INSERT INTO
+  projects.e_project_log (
+    d_operation_type_id,
+    user_id,
+    e_project_id,
+    customer_id,
+    short_name,
+    official_name,
+    description,
+    plan_start_date,
+    plan_end_date,
+    plan_budget,
+    fact_start_date,
+    fact_end_date,
+    fact_budget,
+    project_manager_id,
+    state_id,
+    is_deleted
+  )
+VALUES (
+  2,
+  v_user_id,
+  (SELECT id FROM upd),
+  (SELECT customer_id FROM upd),
+  (SELECT short_name FROM upd),
+  (SELECT official_name FROM upd),
+  (SELECT description FROM upd),
+  (SELECT plan_start_date FROM upd),
+  (SELECT plan_end_date FROM upd),
+  (SELECT fact_start_date FROM upd),
+  (SELECT fact_end_date FROM upd),
+  (SELECT fact_budget FROM upd),
+  (SELECT project_manager_id FROM upd),
+  (SELECT state_id FROM upd),
+  (SELECT is_deleted FROM upd)
+)
 RETURNING
   e_project_id;
 $$ LANGUAGE sql;
