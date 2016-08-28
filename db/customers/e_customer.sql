2e67584190c8e291287204d1db6773d905019162
mtvspec <mtv.spec@gmail.com>
2016-08-28 09:25:31 +0600

    changes name and description attributes and adds e_organization_id attribute

--- db/customers/e_customer.sql
+++ db/customers/e_customer.sql
@@ -15,12 +15,14 @@ CREATE TABLE customers.e_customer_log (
   operation_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT LOCALTIMESTAMP,
   user_id INTEGER NOT NULL,
   e_customer_id INTEGER NOT NULL,
-  name VARCHAR (1000) NOT NULL,
-  description VARCHAR (4000),
-  is_deleted CHAR (1) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (d_operation_type_id) REFERENCES system.d_operation_type (id),
-    FOREIGN KEY (user_id) REFERENCES users.e_user (id),
-    FOREIGN KEY (e_customer_id) REFERENCES customers.e_customer (id),
-    FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
+  e_organization_id INTEGER NOT NULL,
+  a_name VARCHAR (1000) NOT NULL,
+  a_description VARCHAR (4000),
+    is_deleted CHAR (1) NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (d_operation_type_id) REFERENCES system.d_operation_type (id),
+      FOREIGN KEY (user_id) REFERENCES users.e_user (id),
+      FOREIGN KEY (e_customer_id) REFERENCES customers.e_customer (id),
+      FOREIGN KEY (e_organization_id) REFERENCES organizations.e_organization (id),
+      FOREIGN KEY (is_deleted) REFERENCES system.is_deleted (id)
 );
