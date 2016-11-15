f59e11a984501e07c3a38e598e2a5fcd3dfb7a88
mtvspec <mtv.spec@gmail.com>
2016-11-15 23:57:30 +0600

    ...

--- backend-old/insert.js
+++
@@ -1,9 +0,0 @@
-function insert(table, columns, values) {
-  if (!table) {
-    return 'Table name is required';
-  }
-  if (!values) {
-    return 'Values is required'
-  }
-  return 'insert into' + table + '()'
-}
--- backend-old/routers/persons/persons.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const multer = require('multer');
-const upload = multer();
-
-
-exports.persons = persons;
