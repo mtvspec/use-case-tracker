dff9d67a62fcac5a21a6a2d2c856bb34e677db57
mtvspec <mtv.spec@gmail.com>
2016-11-16 22:25:57 +0600

    updates to upper case

--- backend/db/stakeholders/v_stakeholders.sql
+++ backend/db/stakeholders/v_stakeholders.sql
@@ -1,12 +1,13 @@
-create view stakeholders.v_stakeholders
-as
-select
-p.first_name ||' '|| p.last_name "Заинтересованное лицо",
-s.description "Описание"
-from
-stakeholders.e_stakeholder s,
-persons.e_person p
-where
-s.e_person_id = p.id
-order by
-s.id asc;
+CREATE VIEW stakeholders.v_stakeholders
+AS
+SELECT
+  p.first_name ||' '|| p.last_name "Заинтересованное лицо",
+  s.description "Описание"
+FROM
+  stakeholders.e_stakeholder s,
+  persons.e_person p
+WHERE
+  s.e_person_id = p.id
+ORDER BY
+  s.id
+ASC;
