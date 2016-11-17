1d56287e3263b5e8eb3933b97c64a81ada58e47a
mtvspec <mtv.spec@gmail.com>
2016-11-18 01:57:46 +0600

    updates project object specification

---
+++ backend/db/documents/e_document.sql
@@ -0,0 +1,7 @@
+CREATE TABLE documents.e_document (
+  id SERIAL NOT NULL,
+  a_document_name VARCHAR (1000) NOT NULL,
+    PRIMARY KEY (
+      id
+    )
+);
--- backend/db/projects/e_project.sql
+++ backend/db/projects/e_project.sql
@@ -41,27 +41,28 @@ CREATE TABLE projects.e_project (
 );
 
 CREATE TABLE projects.e_project (
-  id SERIAL,
+  id SERIAL NOT NULL,
   e_customer_id INTEGER,
-  a_name VARCHAR (1000) NOT NULL,
-  a_description VARCHAR (4000),
+  a_project_name VARCHAR (1000) NOT NULL,
+  a_project_description VARCHAR (4000),
+  a_official_project_name VARCHAR (4000),
+  a_plan_start_date DATE,
+  a_plan_end_date DATE,
+  a_plan_budget NUMERIC,
+  a_fact_start_date DATE,
+  a_fact_end_date DATE,
+  a_fact_budget NUMERIC,
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
       PRIMARY KEY (
+        a_project_name,
+        a_project_description
+      ),
+      UNIQUE (
         id
       ),
       FOREIGN KEY (
         e_customer_id
-      ) REFERENCES customers.e_customer (id),
-      UNIQUE (
-        a_name
-      ),
-      UNIQUE (
-        a_description
-      ),
-      UNIQUE (
-        a_name,
-        a_description
-      )
+      ) REFERENCES customers.e_customer (id)
 );
 
 CREATE TABLE projects.e_project_log (
@@ -113,11 +114,13 @@ CREATE TABLE projects.o_create_project (
       e_project_id
     ) REFERENCES projects.e_project (id)
 );
-
+--============================================================================--
+-- Create project
+--============================================================================--
 CREATE FUNCTION projects.create_project (
   IN v_e_customer_id INTEGER DEFAULT NULL,
-  IN v_a_name VARCHAR (1000) DEFAULT NULL,
-  IN v_a_description VARCHAR (4000) DEFAULT NULL,
+  IN v_a_project_name VARCHAR (1000) DEFAULT NULL,
+  IN v_a_project_description VARCHAR (4000) DEFAULT NULL,
   IN v_e_user_id INTEGER DEFAULT NULL,
   OUT v_e_project_id INTEGER
 )
@@ -126,14 +129,14 @@ WITH ins AS (
   INSERT INTO
     projects.e_project (
       e_customer_id,
-      a_name,
-      a_description
+      a_project_name,
+      a_project_description
     )
   VALUES
   (
     v_e_customer_id,
-    v_a_name,
-    v_a_description
+    v_a_project_name,
+    v_a_project_description
   )
   RETURNING
     id
@@ -156,15 +159,117 @@ $$ LANGUAGE sql;
 
 SELECT
   projects.create_project (
-    v_a_name := 'New project',
+    v_a_project_name := 'New project',
+    v_a_project_description := 'New project is...',
     v_e_user_id := 1
+  );
+--============================================================================--
+-- Start project
+--============================================================================--
+CREATE TABLE projects.o_start_project (
+  id SERIAL NOT NULL,
+  d_operation_type_id INTEGER NOT NULL,
+  e_user_id INTEGER NOT NULL,
+  e_project_id INTEGER NOT NULL,
+  e_customer_id INTEGER NOT NULL,
+  e_contract_id INTEGER,
+  e_project_manager_id INTEGER NOT NULL,
+  e_project_plan_id INTEGER NOT NULL,
+    PRIMARY KEY (
+      e_project_id,
+      e_customer_id,
+      e_project_manager_id
+    ),
+    UNIQUE (
+      id
+    ),
+    FOREIGN KEY (
+      e_project_id
+    ) REFERENCES projects.e_project (id),
+    FOREIGN KEY (
+      e_customer_id
+    ) REFERENCES customers.e_customer (id),
+    FOREIGN KEY (
+      e_contract_id -- TODO: checking for contract
+    ) REFERENCES documents.e_document (id),
+    FOREIGN KEY (
+      e_project_manager_id
+    ) REFERENCES emp.e_emp (id),
+    FOREIGN KEY (
+      e_project_plan_id -- TODO: check for plan
+    ) REFERENCES documents.e_document (id)
+);
+--------------------------------------------------------------------------------
+CREATE FUNCTION projects.start_project (
+  IN v_e_project_id INTEGER DEFAULT NULL,
+  IN v_e_customer_id INTEGER DEFAULT NULL,
+  IN v_e_contract_id INTEGER DEFAULT NULL,
+  IN v_e_project_manager_id INTEGER DEFAULT NULL,
+  IN v_e_project_plan_id INTEGER DEFAULT NULL,
+  IN v_a_official_project_name VARCHAR (4000) DEFAULT NULL,
+  IN v_a_plan_start_date DATE DEFAULT NULL,
+  IN v_a_plan_end_date DATE DEFAULT NULL,
+  IN v_a_plan_budget NUMERIC DEFAULT NULL,
+  IN v_e_user_id INTEGER DEFAULT NULL,
+  OUT v_operation_id INTEGER
+)
+AS $$
+WITH upd AS (
+  UPDATE
+    projects.e_project
+  SET
+    a_official_project_name = v_a_official_project_name,
+    a_plan_start_date = v_a_plan_start_date,
+    a_plan_end_date = v_a_plan_end_date,
+    a_plan_budget = v_a_plan_budget
+  WHERE
+    id = v_e_project_id
+  RETURNING
+    id,
+    a_official_project_name,
+    a_plan_start_date,
+    a_plan_end_date,
+    a_plan_budget
+)
+INSERT INTO
+  projects.o_start_project (
+    d_operation_type_id,
+    e_user_id,
+    e_project_id,
+    e_customer_id,
+    e_contract_id,
+    e_project_manager_id,
+    e_project_plan_id
+  )
+VALUES
+(
+  2,
+  v_e_user_id,
+  (SELECT id FROM upd),
+  v_e_customer_id,
+  v_e_contract_id,
+  v_e_project_manager_id,
+  v_e_project_plan_id
+)
+RETURNING
+  id "v_operation_id";
+$$ LANGUAGE sql;
+
+SELECT
+  projects.start_project (
+    v_e_user_id := 1,
+    v_e_project_id := 3,
+    v_e_customer_id := 1,
+    v_e_project_manager_id := 1,
+    v_e_project_plan_id := 1,
+    v_e_contract_id := 2
   );
 
 CREATE VIEW projects.v_project_operations
 AS
 SELECT
   p.id "Код",
-  p.a_name "Проект",
+  p.a_project_name "Проект",
   o.operation_en "Операция",
   pn.first_name ||' '|| pn.last_name "Пользователь",
   cr.a_operation_timestamp "Штамп времени"
@@ -173,7 +278,8 @@ FROM
   projects.e_project_operation o,
   persons.e_person pn,
   users.e_user u,
-  projects.o_create_project cr
+  projects.o_create_project cr,
+  projects.o_start_project st
 WHERE
   cr.e_project_id = p.id
 AND
@@ -182,6 +288,12 @@ AND
   cr.e_user_id = u.id
 AND
   u.e_person_id = pn.id
+AND
+  st.e_project_id = p.id
+AND
+  st.d_operation_type_id = o.id
+AND
+  st.e_user_id = u.id
 ORDER BY
   p.id
 ASC;
