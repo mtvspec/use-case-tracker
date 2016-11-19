6650ebe23ab30b09222d745880df4770414937af
mtvspec <mtv.spec@gmail.com>
2016-11-19 10:11:51 +0600

    updates defect object specification

--- backend/db/defects/e_defect.sql
+++ backend/db/defects/e_defect.sql
@@ -1,3 +1,6 @@
+--============================================================================--
+-- Defect
+--============================================================================--
 CREATE TABLE defects.e_defect (
   id SERIAL NOT NULL,
   d_defect_kind_id INTEGER NOT NULL DEFAULT 1,
@@ -29,66 +32,7 @@ CREATE TABLE defects.e_defect (
         e_subject_id
       ) REFERENCES defects.e_subject (id),
       FOREIGN KEY (
-        d_defect_status_id
-      ) REFERENCES defects.d_defect_status (id)
+        d_defect_state_id
+      ) REFERENCES defects.d_defect_state (id)
 );
-
-CREATE VIEW defects.v_defects
-AS
-SELECT
-  d.id "Код",
-  s.a_name "Тема",
-  d.a_name "Наименование",
-  d.a_description "Описание",
-  c.status "Статус"
-FROM
-  defects.e_defect d,
-  defects.e_subject s,
-  defects.d_defect_status c
-WHERE
-  d.subject_id = s.id
-AND
-  d.status_id = c.id
-ORDER BY
-  d.id
-ASC;
-
-
-SELECT
-  s.a_name "Тема",
-  d.a_name "Наименование",
-  d.a_description "Описание"
-FROM
-  defects.e_defect d,
-  defects.e_subject s,
-  defects.d_defect_status c
-WHERE
-  d.subject_id = s.id
-AND
-  d.status_id = c.id
-ORDER BY
-  d.id
-ASC;
-
-CREATE VIEW defects.v_defects
-AS
-SELECT
-  d.id "Код",
-  ct.a_name "Компонент",
-  s.a_name "Тема",
-  d.a_name "Наименование",
-  d.a_description "Описание"
-FROM
-  defects.e_defect d,
-  defects.e_subject s,
-  components.e_component ct,
-  defects.d_defect_status c
-WHERE
-  d.subject_id = s.id
-AND
-  d.e_component_id = ct.id
-AND
-  d.status_id = c.id
-ORDER BY
-  d.id
-ASC;
+--------------------------------------------------------------------------------
