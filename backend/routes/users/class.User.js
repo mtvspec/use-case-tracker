3bcd6aafba2510015dcac4426cb7208674ec9a4b
mtvspec <mtv.spec@gmail.com>
2016-11-23 01:15:12 +0600

    updates user and test case object specifications

--- backend/routes/users/class.User.js
+++ backend/routes/users/class.User.js
@@ -1,7 +1,87 @@
 'use strict';
 
-module.exports = class User {
+const bcrypt = require('bcrypt-nodejs');
+
+/**
+ * @desc This function takes user's credentials (username and password) and
+ * validates it (for availability). If username is available and valid
+ * function creates new User and return it.
+ * @param credentials
+ * @return User
+ */
+class User {
   constructor(data) {
-    
+    let User = {};
+    let messages = {};
+    let result = {};
+    if (!data) {
+      messages.message = `'user data' is required`;
+      return this.result = messages;
+    } else {
+      if (data.username) {
+        if (checkUsername(data.username)) {
+          User.username = data.username;
+        } else {
+          messages.username = `invalid username: '${data.username}'`;
+        }
+      } else {
+        messages.username = `'username' is required`;
+      }
+      if (data.password) {
+        if (checkPassword(data.password)) {
+          // TODO: hash password
+          User.password = bcrypt.hashSync(data.password);
+        } else {
+          messages.password = `invalid password: '${data.password}'`;
+        }
+      } else {
+        messages.password = `'password' is required`;
+      }
+      if (Object.keys(messages).length > 0) {
+        return this.result = messages;
+      } else {
+        return this.result = User;
+      }
+    }
+  }
+}
+
+let u = new User({
+  username: 'mtvspec',
+  password: 'mtvspec'
+});
+
+console.log(u);
+
+function createUser(User) {
+  if (!User) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function checkUsername(username) {
+  if (!username) {
+    return false;
+  } else {
+    if (typeof username === 'string' && username.length > 6) {
+      // TODO: check username availability
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+function checkPassword(password) {
+  if (!password) {
+    return false;
+  } else {
+    if (typeof password === 'string' && password.length > 6) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
