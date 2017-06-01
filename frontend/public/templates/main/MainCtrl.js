2e08ac58ae4e66b3fbea84a1f90ee167c187b06a
mtvspec <mtv.spec@gmail.com>
2017-06-01 10:31:21 +0600

    removes logging

--- frontend/public/templates/main/MainCtrl.js
+++ frontend/public/templates/main/MainCtrl.js
@@ -2,16 +2,13 @@
   'use strict';
 
   app.controller('MainCtrl', function MainCtrl($scope, $http, $state) {
-    console.log('MainCtrl');
     $scope.logOut = function (data) {
       $http({
         method: 'POST',
         url: '/api/users/logout',
         data: data
       }).then(function success(response) {
-        console.log(response);
         if (response && response.status === 200) {
-          console.log('ok');
           $state.go('login');
         }
       }, function failure(response) {
