7e72a509d9d1f772ba29f08af634239e4a1c3edd
mtvspec <mtv.spec@gmail.com>
2017-06-01 12:18:05 +0600

    some refactoring

--- frontend/public/templates/main/MainCtrl.js
+++ frontend/public/templates/main/MainCtrl.js
@@ -2,11 +2,10 @@
   'use strict';
 
   app.controller('MainCtrl', function MainCtrl($scope, $http, $state) {
-    $scope.logOut = function (data) {
+    $scope.logOut = function () {
       $http({
         method: 'POST',
-        url: '/api/users/logout',
-        data: data
+        url: '/api/users/logout'
       }).then(function success(response) {
         if (response && response.status === 200) {
           $state.go('login');
