2b72e40ad308c79db14765e71d6cfcf4347314ea
mtvspec <mtv.spec@gmail.com>
2017-06-06 22:08:26 +0600

    updates updatePerson service

--- backend/routes/persons/index.js
+++ backend/routes/persons/index.js
@@ -31,19 +31,41 @@ router
 .post('/', function (req, res) {
   PersonAPI.createPerson(req, res);
 })
-.put('/:id', function (req, res) {
-  let person = {
-    id: req.params.id,
-    data: req.body
-  }
-  PersonAPI.updatePerson(req.session, person, function(response) {
     return res
     .status(response.status)
     .json(response.data)
     .end();
   });
 })
+.put('/:id', function (req, res) {
+  if (validate.is.positive(req.params.id) && validate.is.object(req.body)) {
+    req.body.id = req.params.id;
+    PersonAPI.updatePerson({
+      person: req.body,
+      sessionID: req.session.sessionID,
+      userID: req.session.userID
+    }, function(response) {
       return res
+        .status(response.status)
+        .json(response.data)
+        .end();
+    });
+  } else if (validate.is.positive(req.params.id) === false) {
+    console.error(new Error(`id '${req.params.id}' is invalid`));
+    return res
+      .status(400)
+      .end(`id '${req.params.id}' is invalid`);
+  } else if (validate.is.object(req.body) === false) {
+    console.error(new Error(`Person data '${req.body}' is invalid`));
+    return res
+      .status(400)
+      .end(`id '${req.body}' is invalid`);
+  } else {
+    return res
+    .status(500)
+    .end();
+  }
+})
 /**
  * @desc This function logically deletes person by it's id
  * @method Delete person by personID
