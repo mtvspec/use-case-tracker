99eb901610777545a141d7336aeff8d8bf792ffe
mtvspec <mtv.spec@gmail.com>
2017-06-08 16:26:47 +0600

    Adds fields document description, document type and foreign key constraint references to dictionary

--- backend/db/documents/e_document.sql
+++ backend/db/documents/e_document.sql
@@ -4,8 +4,14 @@
 CREATE TABLE documents.e_document (
   id BIGSERIAL,
   a_document_name VARCHAR (1000) NOT NULL,
-    PRIMARY KEY (
-      id
-    )
+  a_document_desc TEXT,
+  d_document_type_id INTEGER NOT NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+      PRIMARY KEY (
+        id
+      ),
+      FOREIGN KEY (
+        d_document_type_id
+      ) REFERENCES dict.e_dict_value (id)
 );
 --============================================================================--
