7537356f7fb00767a40507cd5386144d648991b2
mtvspec <mtv.spec@gmail.com>
2017-06-16 12:44:10 +0600

    some refactoring

---
+++ db/system/dict.sql
@@ -0,0 +1,68 @@
+--============================================================================--
+-- System kind (d_system_kind)
+--============================================================================--
+INSERT INTO dict.e_dict_value (
+    e_dict_id,
+    a_dict_value_name_en,
+    a_dict_value_name_ru
+) VALUES
+(
+  17,
+  'Information system'
+);
+--============================================================================--
+-- System operation (d_system_operation)
+--============================================================================--
+INSERT INTO dict.e_dict_value (
+    e_dict_id,
+    a_dict_value_name_en,
+    a_dict_value_name_ru
+) VALUES
+(
+  35,
+  'Create system',
+  'Создать систему'
+),
+(
+  35,
+  'Update system',
+  'Обновить данные системы'
+),
+(
+  35,
+  'Delete system',
+  'Удалить систему'
+),
+(
+  35,
+  'Restore system',
+  'Восстановить удаленную систему'
+);
+--============================================================================--
+-- System State (d_system_state)
+--============================================================================--
+INSERT INTO dict.e_dict_value (
+    e_dict_id,
+    a_dict_value_name_en,
+    a_dict_value_name_ru
+) VALUES
+(
+  19,
+  'Created',
+  'Создана'
+);
+--============================================================================--
+-- System Type (d_system_type)
+--============================================================================--
+INSERT INTO dict.e_dict_value (
+    e_dict_id,
+    a_dict_value_name_en,
+    a_dict_value_name_ru
+) VALUES
+(
+  3,
+  'Information system',
+  'Информационная система'
+);
+--============================================================================--
+
