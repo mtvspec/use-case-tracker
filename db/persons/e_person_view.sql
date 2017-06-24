315bd682631dbc4d601b77c1bd14d840aba5deac
mtvspec <mtv.spec@gmail.com>
2017-06-24 17:44:30 +0600

    changes db column names to camelcase

--- db/persons/e_person_view.sql
+++ db/persons/e_person_view.sql
@@ -2,13 +2,13 @@ CREATE OR REPLACE VIEW persons.e_person_view
 AS
 SELECT
   id,
-  a_person_iin "aPersonIIN",
-  a_person_last_name "aPersonLastName",
-  a_person_first_name "aPersonFirstName",
-  a_person_middle_name "aPersonMiddleName",
-  to_char(a_person_dob, 'DD.MM.YYYY') "aPersonDOB",
-  d_person_gender_id "dPersonGenderID",
-  is_deleted "isDeleted"
+  "aPersonIIN",
+  "aPersonLastName",
+  "aPersonFirstName",
+  "aPersonMiddleName",
+  "aPersonDOB",
+  "dPersonGenderID",
+  "isDeleted"
 FROM
   persons.e_person
 ORDER BY
