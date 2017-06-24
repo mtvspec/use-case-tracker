0c36fda0614fbf54f1570a2d05713b3534e5838d
mtvspec <mtv.spec@gmail.com>
2017-06-24 17:20:28 +0600

    deletes old states and makes states a child states of state main

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -5,173 +5,113 @@ let app = angular.module('app', ['ui.router', 'ui.bootstrap']);
 app.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
-  .state('login', {
-    url: '/login',
-    templateUrl: '/templates/login/login.html',
-    controller: 'LoginCtrl',
-    data: {
-      pageTitle: 'Authorization'
-    }
-  })
-  .state('main', {
-    url: '/main',
-    templateUrl: '/templates/main/main.html',
-    controller: 'MainCtrl',
-    controllerAs: 'vm'
-  })
-  .state('main.projects', {
-    name: 'projects',
-    url: '/projects',
-    templateUrl: '/components/projects/views/template.html',
-    controller: 'ProjectsCtrl',
-    controllerAs: 'vm'
-  })
-  .state('createPerson', {
-    url: '/createPerson',
-    templateUrl: '/components/persons/CreatePerson/views/template.html',
-    controller: 'CreatePersonCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Create person'
-    }
-  })
-  .state('createDefectReport', {
-    url: '/createDefectReport',
-    templateUrl: '/components/defect-reports/createDefectReport/views/template.html',
-    controller: 'CreateDefectReportCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Create defect report'
-    }
-  })
-  .state('createUseCaseSubject', {
-    url: '/createUseCaseSubject',
-    templateUrl: '/components/use-case-subjects/createUseCaseSubject/views/template.html',
-    controller: 'CreateUseCaseSubjectCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Create use-case subject'
-    }
-  })
-  .state('OrgChart', {
-    url: '/orgChart',
-    templateUrl: '/components/org-chart/template.html',
-    controller: 'OrgChatCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'OrgChart'
-    }
-  })
-  .state('createUseCaseSlice', {
-    url: '/createUseCaseSlice',
-    templateUrl: '/components/use-case-slices/createUseCaseSlice/views/template.html',
-    controller: 'CreateUseCaseSliceCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Create use-case slice'
-    }
-  })
-  .state('systems', {
-    url: '/systems',
-    templateUrl: '/components/systems/views/template.html',
-    controller: 'SystemsCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Systems'
-    }
-  })
-  .state('projects', {
-    url: '/projects',
-    templateUrl: '/components/projects/views/template.html',
-    controller: 'ProjectsCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Projects'
-    }
-  })
-  .state('createProject', {
-    url: '/createProject',
-    controller: 'CreateProjectCtrl',
-    controllerAs: 'vm',
-    templateUrl: '/components/projects/createProject/views/template.html',
-  })
-  .state('createSystem', {
-    url: '/createSystem',
-    templateUrl: '/components/systems/views/createSystem.html',
-    controller: 'CreateSystemCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Create system'
-    }
-  })
-  .state('customers', {
-    url: '/customers',
-    templateUrl: '/components/customers/customersList/views/template.html',
-    controller: 'CustomersCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Customers'
-    }
-  })
-  .state('issues', {
-    url: '/issues',
-    templateUrl: '/components/issues/issuesList/views/template.html',
-    controller: 'IssuesListCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Issues'
-    }
-  })
-  .state('tracker', {
-    url: '/tracker',
-    templateUrl: '/components/use-case-subjects/UseCaseSubjectList/views/template.html',
-    controller: 'UseCaseSubjectListCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Use-case subjects'
-    }
-  })
-  .state('use-case-slices', {
-    url: '/use-case-slices',
-    templateUrl: '/templates/use-case-slices/template.html',
-    controller: 'UseCaseSliceCtrl',
-    controllerAs: 'vm'
-  })
-  .state('organizations', {
-    url: '/organizations',
-    templateUrl: '/components/organizations/OrganizationsList/views/template.html',
-    controller: 'OrganizationsCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Organizations'
-    }
-  })
-  .state('persons', {
-    url: '/persons',
-    templateUrl: '/components/persons/PersonsList/views/template.html',
-    controller: 'PersonsListCtrl',
-    controllerAs: 'vm',
-    data: {
-      pageTitle: 'Persons'
-    }
-  })
-  .state('contacts', {
-    url: '/contacts',
-    templateUrl: 'contacts.html'
-  });
+    .state('login', {
+      url: '/login',
+      templateUrl: '/templates/login/login.html',
+      controller: 'LoginCtrl',
+      data: {
+        pageTitle: 'Authorization'
+      }
+    })
+    .state('main', {
+      url: '/main',
+      templateUrl: '/templates/main/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'vm'
+    })
+    .state('main.projects', {
+      name: 'projects',
+      url: '/projects',
+      templateUrl: '/components/projects/views/template.html',
+      controller: 'ProjectsCtrl',
+      controllerAs: 'vm'
+    })
+    .state('main.persons', {
+      url: '/persons',
+      templateUrl: '/components/persons/PersonsList/views/template.html',
+      controller: 'PersonsListCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Persons'
+      }
+    })
+    .state('OrgChart', {
+      url: '/orgChart',
+      templateUrl: '/components/org-chart/template.html',
+      controller: 'OrgChatCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'OrgChart'
+      }
+    })
+    .state('main.systems', {
+      url: '/systems',
+      templateUrl: '/components/systems/views/template.html',
+      controller: 'SystemsCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Systems'
+      }
+    })
+    .state('main.customers', {
+      url: '/customers',
+      templateUrl: '/components/customers/customersList/views/template.html',
+      controller: 'CustomersCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Customers'
+      }
+    })
+    .state('main.issues', {
+      url: '/issues',
+      templateUrl: '/components/issues/issuesList/views/template.html',
+      controller: 'IssuesListCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Issues'
+      }
+    })
+    .state('main.tracker', {
+      url: '/tracker',
+      templateUrl: '/components/use-case-subjects/UseCaseSubjectList/views/template.html',
+      controller: 'UseCaseSubjectListCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Use-case subjects'
+      }
+    })
+    .state('main.use-case-slices', {
+      url: '/use-case-slices',
+      templateUrl: '/templates/use-case-slices/template.html',
+      controller: 'UseCaseSliceCtrl',
+      controllerAs: 'vm'
+    })
+    .state('main.organizations', {
+      url: '/organizations',
+      templateUrl: '/components/organizations/OrganizationsList/views/template.html',
+      controller: 'OrganizationsCtrl',
+      controllerAs: 'vm',
+      data: {
+        pageTitle: 'Organizations'
+      }
+    })
+    .state('main.contacts', {
+      url: '/contacts',
+      templateUrl: 'contacts.html'
+    });
 });
 
 app.directive('title', ['$rootScope', '$timeout',
-  function($rootScope, $timeout) {
+  function ($rootScope, $timeout) {
     return {
-      link: function() {
+      link: function () {
 
-        var listener = function(event, toState) {
+        var listener = function (event, toState) {
 
-          $timeout(function() {
+          $timeout(function () {
             $rootScope.title = (toState.data && toState.data.pageTitle)
-            ? toState.data.pageTitle
-            : 'Default title';
+              ? toState.data.pageTitle
+              : 'Default title';
           });
         };
 
