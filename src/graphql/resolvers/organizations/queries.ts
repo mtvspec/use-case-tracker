1fd97bab755208e2b5eb1a695c7952584ace8b3a
mtvspec <mtv.spec@gmail.com>
2017-11-10 19:30:01 +0600

    some refactoring

--- src/graphql/resolvers/organizations/queries.ts
+++ src/graphql/resolvers/organizations/queries.ts
@@ -2,19 +2,24 @@ import CommonResolvers from './../common'
 import { OrganizationsService } from './../../../services/organizations.service'
 import { EmployeesService } from './../../../services/employees.service'
 import { PersonsService } from './../../../services/persons.service'
-const getOrganizationalUnitByID = async (root, args) => {
+const getOrganizationalUnitByID = async (root: any, args: any) => {
   return await OrganizationsService.getOrganizationalUnitByID(args.id)
 }
 
-const getOrganizationByID = async (root, args) => {
+const getOrganizationByID = async (root: any, args: any) => {
   return await OrganizationsService.getOrganization(args.id)
 }
 
+const getManagerByID = async (root: any) => {
+  return root.managerID ?
+    await EmployeesService.getEmployee(root.managerID) : null
+}
+
 const Organization = {
-  organizationalUnit: async (root) => {
-    return root.organizationalUnitID ?
-      await OrganizationsService.getOrganizationalUnitByOrganizationID(root.organizationalUnitID) : null
+  organizationalUnits: async (root: any) => {
+    return await OrganizationsService.getOrganizationalUnits(root.id)
   },
+  manager: getManagerByID,
   createdBy: CommonResolvers.createdBy,
   updatedBy: CommonResolvers.updatedBy,
   deletedBy: CommonResolvers.deletedBy,
@@ -22,30 +27,30 @@ const Organization = {
 }
 
 const OrganizationsConnection = {
-  totalCount: async (root) => {
+  totalCount: async (root: any) => {
     return await OrganizationsService.getOrganizationsCount()
-      .then((data: { totalCount: number }) => { return data.totalCount })
+      .then((data: any) => { return data.totalCount })
   },
-  organizations: async (root) => {
+  organizations: async (root: any) => {
     return await OrganizationsService.getOrganizations()
   }
 }
 
 const OrganizationalUnit = {
-  manager: async (root) => {
+  manager: async (root: any) => {
     return root.managerID > 0 ?
       await OrganizationsService.getManagerByID(root.managerID) : null
   },
-  managers: async (root) => {
+  managers: async (root: any) => {
     return OrganizationsService.getManagersByOrganizationalUnitID(root.id)
   },
-  childOrganizationalUnits: async (root) => {
+  childOrganizationalUnits: async (root: any) => {
     return await OrganizationsService.getChildOrganizationalUnitsByOrganizationalUnitID(root.id)
   },
-  employees: async (root) => {
+  employees: async (root: any) => {
     return await OrganizationsService.getEmployeesByOrganizationalUnitID(root.id)
   },
-  allEmployees: async (root) => {
+  allEmployees: async (root: any) => {
     return await OrganizationsService.getAllEmployeesByOrganizationalUnitID(root.id)
   },
   createdBy: CommonResolvers.createdBy,
@@ -55,10 +60,6 @@ const OrganizationalUnit = {
 }
 
 const PositionalUnit = {
-  organizationalUnit: async (root) => {
-    return root.organizationalUnitID ?
-      await OrganizationsService.getOrganizationalUnitByID(root.organizationalUnitID) : null
-  },
   createdBy: CommonResolvers.createdBy,
   updatedBy: CommonResolvers.updatedBy,
   deletedBy: CommonResolvers.deletedBy,
@@ -66,42 +67,42 @@ const PositionalUnit = {
 }
 
 const EmployeesConnection = {
-  totalCount: async (root) => {
+  totalCount: async (root: any) => {
     return root.id ?
       await EmployeesService.getEmployeesCount(root.id)
-        .then((data: { totalCount: number }) => { return data.totalCount }) : null
+        .then((data: any) => { return data.totalCount }) : null
   },
-  edges: async (root) => {
+  edges: async (root: any) => {
     return root.id ?
       await EmployeesService.getEmployeesByPersonID(root.id) : null
   }
 }
 
 const PersonEmployeeEdge = {
-  node: async (root) => {
+  node: async (root: any) => {
     return await EmployeesService.getEmployee(root.empID)
   }
 }
 
 const Employee = {
-  person: async (root, args, context, info) => {
-    const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
+  person: async (root: any, args: any, context: any, info: any) => {
+    const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map((selection: any) => selection.name.value)
     return await PersonsService.getPerson(unfilteredFields, root.personID)
   },
-  organizationalUnit: async (root) => {
+  organizationalUnit: async (root: any) => {
     return root.organizationalUnitID ?
       await OrganizationsService.getOrganizationalUnitByID(root.organizationalUnitID) : null
   },
-  positionalUnit: async (root) => {
+  positionalUnit: async (root: any) => {
     return root.positionalUnitID ?
       await OrganizationsService.getPositionalUnitByEmployeeID(root.id) : null
   },
-  manager: async (root) => {
+  manager: async (root: any) => {
     return root.managerID ?
       await EmployeesService.getEmployee(root.managerID) : null
   },
-  subordinates: async (root) => {
-    return await OrganizationsService.getSubordinadedOrganizationalUnitsManagersByOrganizationalUnitID(root.organizationalUnitID)
+  subordinates: async (root: any) => {
+    return await OrganizationsService.getSubordinades(root.id)
   },
   createdBy: CommonResolvers.createdBy,
   updatedBy: CommonResolvers.updatedBy,
