d194ca6e52b027119333e9b3df2a812fd3aaa630
mtvspec <mtv.spec@gmail.com>
2017-11-12 21:29:30 +0600

    adds fields parsing function

--- src/graphql/resolvers/common/index.ts
+++ src/graphql/resolvers/common/index.ts
@@ -1,35 +1,38 @@
 import { UsersService } from './../../../services'
 import { PersonsService } from './../../../services'
-const createdBy = async (root, args, context, info) => {
-  const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
-  return await UsersService.getUser(unfilteredFields, root.createdBy)
+import { DictService } from './../../../services'
+const createdBy = async (root: { createdBy: number }, args: any, ctx: any, info: any) => {
+  const fields = Object.keys(ctx.utils.parseFields(info))
+  return await UsersService.getUser(fields, root.createdBy)
 }
-const updatedBy = async (root, args, context, info) => {
-  const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
+const updatedBy = async (root: { updatedBy: number }, args: any, ctx: any, info: any) => {
+  const fields = Object.keys(ctx.utils.parseFields(info))
   return root.updatedBy > 0 ?
-    await UsersService.getUser(unfilteredFields, root.updatedBy) : null
+    await UsersService.getUser(fields, root.updatedBy) : null
 }
-const deletedBy = async (root, args, context, info) => {
-  const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
+const deletedBy = async (root: { deletedBy: number }, args: any, ctx: any, info: any) => {
+  const fields = Object.keys(ctx.utils.parseFields(info))
   return root.deletedBy > 0 ?
-    await UsersService.getUser(unfilteredFields, root.deletedBy) : null
+    await UsersService.getUser(fields, root.deletedBy) : null
 }
-const modifiedBy = async (root, args, context, info) => {
-  const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
-  return await UsersService.getUser(unfilteredFields, root.modifiedBy)
+const modifiedBy = async (root: { modifiedBy: number }, args: any, ctx: any, info: any) => {
+  const fields = Object.keys(ctx.utils.parseFields(info))
+  return await UsersService.getUser(fields, root.modifiedBy)
 }
-
-const getPersonByUserID = async (root, args, context, info) => {
-  const unfilteredFields = info.fieldNodes[0].selectionSet.selections.map(selection => selection.name.value)
-  return await PersonsService.getPerson(unfilteredFields, root.personID)
+const state = async (root: { state: number }, args: any, ctx: any, info: any) => {
+  const fields: any[] = Object.keys(ctx.utils.parseFields(info))
+  return await DictService.getDictValue(fields, root.state)
 }
-
+const getPersonByUserID = async (root: { person: number }, args: any, ctx: any, info: any) => {
+  const fields: any[] = Object.keys(ctx.utils.parseFields(info))
+  return await PersonsService.getPerson(fields, root.person)
+}
 const CommonResolvers = {
+  state,
   createdBy,
   updatedBy,
   deletedBy,
   modifiedBy,
   getPersonByUserID
 }
-
 export default CommonResolvers
